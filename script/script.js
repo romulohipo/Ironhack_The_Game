db4cbcdc0f6e4cc6415f2d71870e7e03bc9eca91
Romulo Hipolito <hipolito.romulo12@gmail.com>
2021-01-26 22:49:32 -0300

    bug fixes

--- script/script.js
+++ script/script.js
@@ -13,9 +13,9 @@ const player = {
     y: 200,
     width: 32,
     height: 48,
-    framex: 0,
-    framey: 0,
-    speed: 7,
+    frameX: 0,
+    frameY: 0,
+    speed: 3,
     moving: false
 }
 // Player Image //
@@ -35,8 +35,9 @@ function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     //player sprite //
-    drawSprite(playerSprite, player.width * player.framex, player.height * player.framey, player.width, player.height, player.x, player.y, player.width, player.height);
+    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
     movePlayer();
+    handlePlayerFrame();
     requestAnimationFrame(animate);
 }
 
@@ -58,22 +59,26 @@ window.addEventListener("keyup", function(e){
 function movePlayer(){
     if( keys[38] && player.y  > 100){
         player.y -= player.speed;
-        player.framey = 3;
+        player.frameY = 3;
     }
         // Move Left
     if( keys[37] && player.x > 0){
         player.x -= player.speed;
-        player.framey = 1;
+        player.frameY = 1;
     }
         // Move Down
     if( keys[40] && player.y < canvas.height - player.height){
         player.y += player.speed;
-        player.framey = 0;
+        player.frameY = 0;
     }
         // Move Right
     if( keys[39] && player.x < canvas.width - player.width){
         player.x += player.speed;
-        player.framey = 2;
+        player.frameY = 2;
     }
 }
-
+// Controlling player animation Frame //
+function handlePlayerFrame(){
+    if (player.frameX < 3) player.frameX ++
+    else player.frameX = 0;
+}
