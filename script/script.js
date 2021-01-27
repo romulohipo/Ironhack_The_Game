1ef9322db074cd5320e894abbca02bc74efd67ed
Romulo Hipolito <hipolito.romulo12@gmail.com>
2021-01-26 22:14:06 -0300

    moving hero character

--- script/script.js
+++ script/script.js
@@ -9,13 +9,13 @@ const keys = [];
 
 // Player Info //
 const player = {
-    x: 0,
-    y: 0,
+    x: 400,
+    y: 200,
     width: 32,
     height: 48,
     framex: 0,
     framey: 0,
-    speed: 10,
+    speed: 7,
     moving: false
 }
 // Player Image //
@@ -34,9 +34,46 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(playerSprite, 0, 0, player.width, player.height, 200, 200, player.width, player.height);
+    //player sprite //
+    drawSprite(playerSprite, player.width * player.framex, player.height * player.framey, player.width, player.height, player.x, player.y, player.width, player.height);
+    movePlayer();
     requestAnimationFrame(animate);
 }
 
 animate();
+
+//Tracking of pressed keys
+window.addEventListener("keydown", function(e){
+    keys[e.keyCode] =  true;
+    console.log(keys);
+});
+
+window.addEventListener("keyup", function(e){
+    delete keys[e.keyCode];
+});
+
+// Moving Player //
+
+        // Move Up//
+function movePlayer(){
+    if( keys[38] && player.y  > 100){
+        player.y -= player.speed;
+        player.framey = 3;
+    }
+        // Move Left
+    if( keys[37] && player.x > 0){
+        player.x -= player.speed;
+        player.framey = 1;
+    }
+        // Move Down
+    if( keys[40] && player.y < canvas.height - player.height){
+        player.y += player.speed;
+        player.framey = 0;
+    }
+        // Move Right
+    if( keys[39] && player.x < canvas.width - player.width){
+        player.x += player.speed;
+        player.framey = 2;
+    }
+}
 
